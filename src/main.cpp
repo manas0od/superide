71a29886785dc023ae998f80fd8c3bd2c05c4586
manas0od <manasodvaigaod@gmail.com>
2025-12-12 15:55:36 +0530

    Initialize SDL and ImGui for SuperIDE
    Replaced console output with SDL window creation and ImGui setup for a graphical interface.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,63 @@
-#include <iostream>
+#include <SDL2/SDL.h>
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <stdio.h>
 
 int main() {
-    std::cout << "SuperIDE starting...\n";
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SuperIDE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+
+    // ImGui setup
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // ----- IDE UI -----
+        ImGui::Begin("SuperIDE");
+        ImGui::Text("Welcome to SuperIDE!");
+        ImGui::Text("The lightweight, ultra-fast IDE.");
+        ImGui::End();
+
+        // ------------------
+        ImGui::Render();
+        SDL_GL_MakeCurrent(window, gl_context);
+        glClearColor(0.1f, 0.1f, 0.12f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Shutdown
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
